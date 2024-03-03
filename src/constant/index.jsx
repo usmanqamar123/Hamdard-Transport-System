9acef753e46f627b2741cec3b0fee250647704f6
Usman Qamar <usmanqamar672003@gmail.com>
2024-03-03 17:21:06 +0500

    Feature section

--- src/constant/index.jsx
+++ src/constant/index.jsx
@@ -2,6 +2,11 @@ import { Icon1 } from "../assets";
 import { Icon2 } from "../assets";
 import { Icon3 } from "../assets";
 import { Icon4 } from "../assets";
+import { FeatureTransparent } from "../assets";
+import { Applications } from "../assets";
+import { Sheild } from "../assets";
+import { Eye } from "../assets";
+import { Dollar } from "../assets";
 
 const Navlink = [
     {
@@ -54,8 +59,64 @@ const Herosection = [
     },
 ]
 
+const Featuresection =[
+    {
+        id:0,
+        counts:"100",
+        title:"We drive points per day",
+        iconBottom:FeatureTransparent,
+    }, 
+    {
+        id:1,
+        counts:"1.2KM",
+        title:"We cross area per day in Karachi",
+        iconBottom:FeatureTransparent,
+    }, 
+    {
+        id:2,
+        counts:"110",
+        title:"Professional drivers force behind",
+        iconBottom:FeatureTransparent,
+    }, 
+    {
+        id:3,
+        counts:"90",
+        title:"Pick locations Pick & Drop",
+        iconBottom:FeatureTransparent,
+    },
+]
+
+const Featuresection2 =[
+    {
+        id:0,
+        icon:Applications,
+        step:"01",
+        title:"Strict Schedules"
+    }, 
+    {
+        id:1,
+        icon:Sheild,
+        step:"02",
+        title:"Committed to Duty"
+    },
+     {
+        id:2,
+        icon:Eye,
+        step:"03",
+        title:"Continuous Monitoring"
+    }, 
+    {
+        id:3,
+        icon:Dollar,
+        step:"04",
+        title:"Making Dollars"
+    },
+]
+
 
 export {
      Navlink,
-     Herosection
+     Herosection,
+     Featuresection,
+     Featuresection2,
 }
